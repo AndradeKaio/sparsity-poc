843eb497ed8f7e3b41974faf059ac4e1ffce13a3
Kaio Henrique <deandradekaio@gmail.com>
2025-02-26 18:03:50 -0300

    chore: main skeleton

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+using DenseMatrix = vector<vector<double>>;
+
+
+DenseMatrix genMatrix(int m, int n, float sparsity){}
+
+bool sampling(DenseMatrix input, float sparsity){
+  return false;
+}
+
+void covertToTACO(DenseMatrix matrix, Format format){}
+
+
+
+int main (int argc, char *argv[]){
+  return 0;
+}
