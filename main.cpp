b3a483c8e4b3890f4a224850afc73185a11cb77d
Kaio Henrique <deandradekaio@gmail.com>
2025-02-27 11:45:37 -0300

    feat: taco mm

--- main.cpp
+++ main.cpp
@@ -1,57 +1,66 @@
-#include <stdio.h>
-#include <vector>
-#include <taco.h>
 #include <iostream>
+#include <stdio.h>
+#include <taco.h>
+#include <vector>
 
 using namespace std;
 using namespace taco;
 
 using DenseMatrix = vector<vector<double>>;
 
+DenseMatrix genMatrix(int rows, int cols, float sparsity) {
+  DenseMatrix matrix(rows, vector<double>(cols, 0.0));
+  srand(time(0));
 
-DenseMatrix genMatrix(int rows, int cols, float sparsity){
-    DenseMatrix matrix(rows, vector<double>(cols, 0.0));
-    srand(time(0));
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      if ((rand() / (double)RAND_MAX) > sparsity)
+        matrix[i][j] = (rand() % 10) + 1;
 
-    for (int i=0; i<rows; ++i)
-        for (int j=0; j<cols; ++j)
-            if ((rand() / (double)RAND_MAX) > sparsity)
-                matrix[i][j] = (rand() % 10) + 1;
-
-    return matrix;
+  return matrix;
 }
 
-bool sampling(DenseMatrix input, float sparsity){
-  int rows = input.size(); 
+bool sampling(DenseMatrix input, float sparsity) {
+  int rows = input.size();
   int cols = input.empty() ? 0 : input[0].size();
   int count = 0;
-  for (int i=0; i<rows; i++)
-    for (int j=0; j<cols; j++)
-      if(input[i][j] == 0)
+  for (int i = 0; i < rows; i++)
+    for (int j = 0; j < cols; j++)
+      if (input[i][j] == 0)
         count++;
   return static_cast<double>(count) / (rows * cols) >= sparsity;
 }
 
-Tensor<double> convertToTACO(DenseMatrix matrix, Format format){
-  int rows = matrix.size(); 
+Tensor<double> convertToTACO(DenseMatrix matrix, Format format) {
+  int rows = matrix.size();
   int cols = matrix.empty() ? 0 : matrix[0].size();
   Tensor<double> tensor({rows, cols}, sparse);
 
-  for (int i=0; i<rows; i++)
-    for (int j=0; j<cols; j++){
+  for (int i = 0; i < rows; i++)
+    for (int j = 0; j < cols; j++) {
       double x = matrix[i][j];
-      if(x != 0)
+      if (x != 0)
         tensor.insert({i, j}, x);
-  }
+    }
+  tensor.pack();
   return tensor;
 }
 
+Tensor<double> matrixMultiply(Tensor<double> A, Tensor<double> B,
+                              Tensor<double> C) {
+  IndexVar i("i"), j("j"), k("k");
+  A(i, j) = sum(k, B(i, k) * C(k, j));
+  A.evaluate();
+  return A;
+}
 
-
-int main (int argc, char *argv[]){
-  DenseMatrix input = genMatrix(5, 5, 0.3);
-  cout << sampling(input, 0.3) << endl;
-  Tensor<double> t = convertToTACO(input, Format({Dense,Sparse,Sparse}));
-  cout << t << endl;
+int main(int argc, char *argv[]) {
+  DenseMatrix b = genMatrix(5, 5, 0.3);
+  DenseMatrix c = genMatrix(5, 5, 0.3);
+  Tensor<double> B = convertToTACO(b, Format({Sparse, Dense}));
+  Tensor<double> C = convertToTACO(c, Format({Sparse, Dense}));
+  Tensor<double> A({5, 5}, Format({Sparse, Dense}));
+  A = matrixMultiply(A, B, C);
+  cout << A << endl;
   return 0;
 }
